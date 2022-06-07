69e71395ee21315d98d5c035780f3620cdade578
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-06 20:03:52 -0700

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,9 +1,8 @@
 import torch
-from torchvision import transforms
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader 
 
-
-def data_loader(pth):
+def data_loader():
     train_transform = transforms.Compose([transforms.RandomRotation(30),
                                        transforms.RandomResizedCrop(400),
                                        transforms.RandomHorizontalFlip(),
@@ -15,6 +14,17 @@ def data_loader(pth):
                                       transforms.ToTensor(),
                                       transforms.Normalize([0.5, 0.5, 0.5],
                                                            [0.5, 0.5, 0.5])])
+
+    train_set=datasets.Flowers102('flower_data/', download=True, train=True, transform=train_transform)
+    trainset=DataLoader(train_set, batch_size=32, shuffle=True)
+
+    test_set=datasets.Flowers102('flower_data', download=True, train=False, transform=test_transform)
+    testset=DataLoader(test_set, batch_size=32, shuffle=False)
+
+    return trainset, testset
     
+trainset, testset= data_loader()
+print(trainset.shape)
+print(testset.shape)
 
     
